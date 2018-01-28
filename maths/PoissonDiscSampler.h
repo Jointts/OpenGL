1afbd92be7fab3003dc2100c9c512f7c7ee5d394
Joonas <joonaslume@gmail.com>
2018-01-28 22:30:32 +0200

    Big refactoring, cant remember all of it

---
+++ maths/PoissonDiscSampler.h
@@ -0,0 +1,14 @@
+//
+// Created by Joonas on 11/17/2017.
+//
+
+#ifndef GAME_POISSONDISCSAMPLER_H
+#define GAME_POISSONDISCSAMPLER_H
+
+
+class PoissonDiscSampler {
+
+};
+
+
+#endif //GAME_POISSONDISCSAMPLER_H
